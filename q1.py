40a3fea62cfe5ef686c36cba02e523ff660d3c3e
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-01 22:21:52 +1000

    dfa template q1a

--- q1.py
+++ q1.py
@@ -1,7 +1,15 @@
 from automata.fa.dfa import DFA
 
+
 def q1a() -> DFA:
-    return 
+    return DFA(
+        states={},
+        input_symbols={"0", "1"},
+        transitions={},
+        initial_state="",
+        final_states={},
+    )
+
 
 def q1b() -> str:
-    return 
+    return
