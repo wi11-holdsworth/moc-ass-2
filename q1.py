f490c50fab7fff0e2beaadcd8160a4c795b5acd1
Will <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-03 15:02:45 +1000

    question 1

--- q1.py
+++ q1.py
@@ -3,11 +3,15 @@ from automata.fa.dfa import DFA
 
 def q1a() -> DFA:
     return DFA(
-        states={},
+        states={"q0", "q1", "q2"},
         input_symbols={"0", "1"},
-        transitions={},
-        initial_state="",
-        final_states={},
+        transitions={
+            "q0": {"0": "q1", "1": "q0"},
+            "q1": {"0": "q0", "1": "q2"},
+            "q2": {"0": "q2", "1": "q2"},
+        },
+        initial_state="q0",
+        final_states={"q0"},
     )
 
 
