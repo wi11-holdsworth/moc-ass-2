69301229ae69ea1f27581a94ce4a10203e338a6d
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-04 01:30:48 +1000

    working q2b

--- q2.py
+++ q2.py
@@ -1,9 +1,24 @@
-from automata.fa import dfa as DFA, nfa as NFA
+from automata.fa.dfa import DFA
+from automata.fa.nfa import NFA
 
 
 def q2a() -> str:
-    return ""
+    return "01100"
 
 
 def q2b(a: DFA, b: DFA) -> NFA:
-    return
+
+    # for each state in a
+    #   create an epsilon transition to the start state in b
+    #   create an epsilon transition from the accept state in b to the start state of a copy of a
+
+    # remove the accept state of a
+    # remove the accept state of b
+
+    # can assume only one accept state for b
+    additional_transitions = {state: set(b.final_states).pop() for state in a.states}
+
+    return additional_transitions
+
+
+q2b()
