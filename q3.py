fb076c0c111f359cb5ee0bcdd0ad708ed35c30f6
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-04 22:14:29 +1000

    complete q3

--- q3.py
+++ q3.py
@@ -3,14 +3,35 @@ from automata.pda.npda import NPDA
 
 def q3a() -> NPDA:
     return NPDA(
-        states=set(),
-        stack_symbols=set(),
-        transitions=dict(),
-        initial_state="",
-        initial_stack_symbol="",
-        final_states=set(),
+        states={"q0", "q1", "q2", "q3"},
+        input_symbols={"a", "b"},
+        stack_symbols={"#", "X"},
+        transitions={
+            "q0": {
+                "": {"#": {("q1", "#")}, "X": {("q1", "X")}},
+                "a": {"#": {("q0", ("X", "#"))}, "X": {("q0", ("X", "X"))}},
+                "b": {"#": {("q0", ("X", "#"))}, "X": {("q0", ("X", "X"))}},
+            },
+            "q1": {
+                "a": {"#": {("q1", "")}, "X": {("q1", "")}},
+                "b": {"#": {("q2", "")}, "X": {("q2", "")}},
+            },
+            "q2": {
+                "": {"#": {("q3", "")}, "X": {("q2", "")}},
+                "a": {"#": {("q2", "")}, "X": {("q2", "")}},
+                "b": {"#": {("q2", "")}, "X": {("q2", "")}},
+            },
+        },
+        initial_state="q0",
+        initial_stack_symbol="#",
+        final_states={"q3"},
     )
 
 
 def q3b() -> list[tuple[str, list[str]]]:
-    return
+    return [
+        ("A", "BC"),
+        ("B", ["a", "b", "BB"]),
+        ("C", ["DbD"]),
+        ("D", ["a", "b", "", "DD"]),
+    ]
