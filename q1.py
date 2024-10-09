b4ae0c411b4aa0e5894a18179d1e0b66f9a336cc
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-10 00:37:48 +1100

    dfa fix

--- q1.py
+++ q1.py
@@ -3,15 +3,20 @@ from automata.fa.dfa import DFA
 
 def q1a() -> DFA:
     return DFA(
-        states={"q0", "q1", "q2"},
+        states=set(f"q{i}" for i in range(8)),
         input_symbols={"0", "1"},
         transitions={
             "q0": {"0": "q1", "1": "q0"},
-            "q1": {"0": "q0", "1": "q2"},
-            "q2": {"0": "q2", "1": "q2"},
+            "q1": {"0": "q2", "1": "q7"},
+            "q2": {"0": "q1", "1": "q3"},
+            "q3": {"0": "q4", "1": "q3"},
+            "q4": {"0": "q5", "1": "q7"},
+            "q5": {"0": "q4", "1": "q6"},
+            "q6": {"0": "q6", "1": "q6"},
+            "q7": {"0": "q7", "1": "q7"},
         },
         initial_state="q0",
-        final_states={"q0"},
+        final_states={"q5", "q6"},
     )
 
 
