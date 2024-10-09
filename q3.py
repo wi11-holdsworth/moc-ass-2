44d9679a30bec9a5cfba5622e51879747c7a8b5a
Will Holdsworth <83637728+wi11-holdsworth@users.noreply.github.com>
2024-10-10 01:32:50 +1100

    pda now ensures that the first half cannot be empty

--- q3.py
+++ q3.py
@@ -1,5 +1,10 @@
 from automata.pda.npda import NPDA
 
+# transitions:
+# (..., "") = pop from the stack
+# (X, ...) = push X to the stack
+# X: X = no change to the stack
+
 
 def q3a() -> NPDA:
     return NPDA(
@@ -8,7 +13,7 @@ def q3a() -> NPDA:
         stack_symbols={"#", "X"},
         transitions={
             "q0": {
-                "": {"#": {("q1", "#")}, "X": {("q1", "X")}},
+                "": {"#": {("q3", "#")}, "X": {("q1", "X")}},
                 "a": {"#": {("q0", ("X", "#"))}, "X": {("q0", ("X", "X"))}},
                 "b": {"#": {("q0", ("X", "#"))}, "X": {("q0", ("X", "X"))}},
             },
@@ -17,14 +22,13 @@ def q3a() -> NPDA:
                 "b": {"#": {("q2", "")}, "X": {("q2", "")}},
             },
             "q2": {
-                "": {"#": {("q3", "")}, "X": {("q2", "")}},
                 "a": {"#": {("q2", "")}, "X": {("q2", "")}},
                 "b": {"#": {("q2", "")}, "X": {("q2", "")}},
             },
         },
         initial_state="q0",
         initial_stack_symbol="#",
-        final_states={"q3"},
+        final_states={"q2"},
     )
 
 
